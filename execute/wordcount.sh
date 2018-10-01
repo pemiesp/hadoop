4c2b87bb2d6d8f2f8e76b008a04b74400a31e519
Pedro Emilio Espinosa Chavez <pemiesp@gmail.com>
2018-10-01 15:55:48 -0500

    1

--- execute/wordcount.sh
+++ execute/wordcount.sh
@@ -1,5 +1,5 @@
 #Compiles and executes WordCount
-export BATCHD_REPO=~/gitviews/batchadoop
+export BATCHD_REPO=~/hadoop
 cd $BATCHD_REPO/application/WordCount
 mvn package 
 hadoop jar $BATCHD_REPO/application/WordCount/target/WordCount-1.0-SNAPSHOT.jar com.diplomadobd.wordcount.WordCount /user/$USER/wordcount/input /user/$USER/wordcount/output
