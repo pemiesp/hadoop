713ff897d15ac98a6eda5ae9d9e24a733c0d89cd
Pedro Emilio Espinosa Chavez <pemiesp@gmail.com>
2018-11-23 16:22:20 -0600

    Delete createtableforecast.sh

--- execute/createtableforecast.sh
+++
@@ -1,6 +0,0 @@
-#Compiles and executes WordCount
-export BATCHD_REPO=~/gitviews/batchadoop
-cd $BATCHD_REPO/application/RetailForecasting
-mvn package 
-hadoop jar $BATCHD_REPO/application/RetailForecasting/target/RetailForecasting-1.0-SNAPSHOT.jar com.diplomadobd.retailforecasting.driver.Raw2TableDriver /user/$USER/50skusraw /user/$USER/skudemandtbl
-cd ~
